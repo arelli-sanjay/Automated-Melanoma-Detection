26db2b3a0ad0186da519ff1890a3cd05c3bcb9d1
ARELLISANJAY <arellisanjay1105@gmail.com>
2026-07-22 19:23:36 +0530

    Initial commit
    Update model path to use os.path.join for better compatibility.

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 # Load your trained model globally
 MODEL_PATH = 'model/skin.h5'
-model = load_model(MODEL_PATH)
+MODEL_PATH = os.path.join("model", "skin.h5")
 
 @app.route("/")
 @app.route("/first")
